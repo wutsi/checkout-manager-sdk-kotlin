76448d31c7d257e2024b049b71a6ad956e6982da
wutsi <herve.tchepannou@wutsi.com>
2022-11-29 06:15:12 -0500

    make SearchPaymentProviderRequest.country nullable

--- src/main/kotlin/com/wutsi/checkout/manager/dto/SearchPaymentProviderRequest.kt
+++ src/main/kotlin/com/wutsi/checkout/manager/dto/SearchPaymentProviderRequest.kt
@@ -3,7 +3,7 @@ package com.wutsi.checkout.manager.dto
 import kotlin.String
 
 public data class SearchPaymentProviderRequest(
-  public val country: String = "",
+  public val country: String? = null,
   public val number: String? = null,
   public val type: String? = null,
 )
