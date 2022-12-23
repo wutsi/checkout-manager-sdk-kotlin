7b30717e844ea2433ec7384eafebc922f6516986
wutsi <herve.tchepannou@wutsi.com>
2022-12-23 18:14:49 -0500

    search order by product

--- src/main/kotlin/com/wutsi/checkout/manager/dto/SearchOrderRequest.kt
+++ src/main/kotlin/com/wutsi/checkout/manager/dto/SearchOrderRequest.kt
@@ -9,6 +9,7 @@ import kotlin.collections.List
 public data class SearchOrderRequest(
   public val businessId: Long? = null,
   public val customerAccountId: Long? = null,
+  public val productId: Long? = null,
   public val status: List<String> = emptyList(),
   public val createdFrom: OffsetDateTime? = null,
   public val createdTo: OffsetDateTime? = null,
